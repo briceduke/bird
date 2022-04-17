9fc06ea3c0d4909f9efcd9f8b23ea717ad82d83b
Brice Duke <bricewduke@gmail.com>
2022-04-17 00:02:21 -0500

    feat(auth): complete login page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,28 @@
+import moment from 'moment';
 import { NextPage } from 'next';
+import Link from 'next/link';
+
+import { useAppSelector } from '../app/hooks';
 
 const IndexPage: NextPage = () => {
+	const { user } = useAppSelector((state) => state.auth);
+
 	return (
-		<div>
-			<button className="btn">Login</button>
+		<div className="w-screen h-screen flex items-center justify-center">
+			{!user && (
+				<Link href="/login">
+					<button className="btn">Login</button>
+				</Link>
+			)}
+
+			{user && (
+				<div className="w-1/2 h-1/2">
+					<h2 className="text-5xl text-primary">{user.displayName}</h2>
+					<h1 className="text-2xl text-secondary">@{user.username}</h1>
+					<p>{user.bio}</p>
+					<p>Joined {moment(user.joinDate).format("MMMM YYYY")}</p>
+				</div>
+			)}
 		</div>
 	);
 };
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,7 +1,103 @@
+import { Field, Form, Formik, FormikHelpers } from 'formik';
 import { NextPage } from 'next';
+import Router from 'next/router';
+import { useState } from 'react';
+import * as Yup from 'yup';
+
+import { useAppDispatch } from '../app/hooks';
+import { useLoginMutation } from '../features/auth/authApi';
+import { setAuth } from '../features/auth/authSlice';
+import { LoginPayload } from '../features/auth/dto/login-payload.dto';
+import { User } from '../models/User';
+
+const LoginSchema = Yup.object().shape({
+	username: Yup.string()
+		.min(3, "3 character minimum length")
+		.max(16, "16 character maximum length")
+		.required("required"),
+	password: Yup.string()
+		.min(8, "8 character minimum length")
+		.required("required"),
+});
 
 const LoginPage: NextPage = () => {
-	return <div>LoginPage</div>;
+	const [login] = useLoginMutation();
+
+	const dispatch = useAppDispatch();
+
+	const [serverError, setServerError] = useState("");
+
+	return (
+		<div>
+			<Formik
+				initialValues={{ username: "", password: "" }}
+				validationSchema={LoginSchema}
+				onSubmit={(
+					values: LoginPayload,
+					{ setSubmitting }: FormikHelpers<LoginPayload>
+				) => {
+					setTimeout(async () => {
+						const res = (await login(values)) as {
+							data?: User;
+							error: { statusCode: number; message: string; error: string };
+						};
+
+						if (res.error) setServerError("invalid credentials!");
+
+						if (res.data) {
+							dispatch(setAuth({ user: res.data }));
+
+							Router.push("/");
+						}
+
+						setSubmitting(false);
+					}, 1000);
+				}}
+			>
+				{({ isSubmitting, errors, touched }) => (
+					<Form className="w-1/6 h-1/3 flex justify-evenly items-center flex-col">
+						<h1 className="text-4xl">Login</h1>
+
+						<div className="w-full">
+							<Field
+								type="text"
+								name="username"
+								className="input input-bordered w-full"
+							/>
+							{errors.username && touched.username ? (
+								<div className="text-error">{errors.username}</div>
+							) : null}
+						</div>
+
+						<div className="w-full">
+							<Field
+								type="password"
+								name="password"
+								className="input input-bordered w-full"
+							/>
+							{errors.password && touched.password ? (
+								<div className="text-error">{errors.password}</div>
+							) : null}
+						</div>
+
+						<button
+							type="submit"
+							disabled={isSubmitting}
+							className={`btn ${isSubmitting ? "loading" : ""}`}
+						>
+							Login
+						</button>
+
+						{serverError && (
+							<div className="text-error">
+								<p>{serverError}</p>
+							</div>
+						)}
+					</Form>
+				)}
+			</Formik>
+		</div>
+	);
 };
 
 export default LoginPage;
